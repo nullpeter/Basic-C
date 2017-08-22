00a0b9fa0a9a26b308adfe4bbcd8b3f6cc39e4ca
Peter Pyne <pyne.peter@gmail.com>
2017-08-21 21:51:00 -0400

    Changed fahrenheit to use floating point numbers.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Code from the book 'The C Programming Language' by Brian W. Kernighan and Dennis
 
 File index:
 
-hello_world.c - Prints 'hello world' - From Section 1.1
+hello_world.c - Prints 'hello world' - From Section 1.1  
 fahrenheit.c - Prints a Fahrenheit-Celsius table - From Section 1.2
--- fahrenheit.c
+++ fahrenheit.c
@@ -4,11 +4,11 @@
 
 #include <stdio.h>
 /* print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300 */
+ * for fahr = 0, 20, ..., 300; floating-point version */
 
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
@@ -17,8 +17,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = (5 / 9) * (fahr-32);
-        printf("%3d %6d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }

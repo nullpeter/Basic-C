c7c86e70636a2ee50a2ecfcaf6ac4ed3aa041d15
Peter Pyne <pyne.peter@gmail.com>
2017-08-25 22:13:34 -0400

    Initial commit

---
+++ 1.3 The For Statement.c
@@ -0,0 +1,14 @@
+//
+// Created by Peter Pyne on 8/25/2017.
+//
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table */
+main()
+{
+    int fahr;
+
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}

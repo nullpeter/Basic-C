eed462a90cdf714cf98cfb6273fea1f15f5c85ef
Peter Pyne <pyne.peter@gmail.com>
2017-08-26 10:22:57 -0400

    Added comparison to 0 or 1 for Exercise 1-6

--- 1.5 Character Input and Output.c
+++ 1.5 Character Input and Output.c
@@ -8,7 +8,9 @@ main()
 {
     int c;
 
-    while ((c = getchar()) != EOF) {
+    while (((c = getchar()) != EOF) == 1) { /* Here I compare the value of original expression to 0 or 1 to verify
+                                             * that the expression getchar() != EOF is 0 or 1. I don't know how to
+                                             * print it otherwise. As per Exercise 1-6 */
         putchar(c);
     }
 }

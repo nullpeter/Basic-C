0ad315e7071ad06c14987699603f799f6cfcab4b
Peter Pyne <pyne.peter@gmail.com>
2017-08-25 21:16:34 -0400

    Added header as per Exercise 1-3

--- fahrenheit.c
+++ fahrenheit.c
@@ -4,7 +4,8 @@
 
 #include <stdio.h>
 /* print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300; floating-point version */
+ * for fahr = 0, 20, ..., 300; floating-point version
+ * Adding header above table */
 
 main()
 {
@@ -16,9 +17,12 @@ main()
     step = 20;      /* step size */
 
     fahr = lower;
+
+    printf("Fahrenheit\tCelsius\n");
+
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%.0f\t\t%.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }

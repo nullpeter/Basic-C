51dfb3fbde7e2cd1dbec5f864134d3006d3d6911
Peter Pyne <pyne.peter@gmail.com>
2017-08-25 22:16:26 -0400

    Reversed output as per Exercise 1-5

--- 1.3 The For Statement.c
+++ 1.3 The For Statement.c
@@ -4,11 +4,13 @@
 
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table */
+/* print Fahrenheit-Celsius table
+ * Reverse version (Exercise 1-5)
+ * */
 main()
 {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }

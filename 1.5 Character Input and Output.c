3cd3dada96edbecd57ba433f186ffdeadf0ed789
Peter Pyne <pyne.peter@gmail.com>
2017-08-26 09:55:48 -0400

    Initial commit

---
+++ 1.5 Character Input and Output.c
@@ -0,0 +1,16 @@
+//
+// Created by Peter Pyne on 8/26/2017.
+//
+#include <stdio.h>
+
+/* copy input to output; 1st version (1.5.1) */
+main()
+{
+    int c;
+
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+}

07cc03853b2ef603b49e83c0556ab641b214bbf1
Peter Pyne <pyne.peter@gmail.com>
2017-08-27 10:08:16 -0400

    Drastically modified for ex 1-12. See file history.

--- 1.5.4 Word Counting.c
+++ 1.5.4 Word Counting.c
@@ -4,30 +4,17 @@
 
 #include <stdio.h>
 
-#define IN 1    /* inside a word */
-#define OUT 0   /* outside a word */
-
 /* count lines, words, and characters in input */
-/* For exercise 1-11, I'm going to test by doing alt-codes, very long inputs from paste. No input. */
-/* Nothing I did seems to break it. */
-
+/* For exercise 1-12, I'm going to print input one word per line */
 
 main()
 {
-    int c, nl, nw, nc, state;
+    int c;
 
-    state = OUT;
-    nl = nw = nc = 0;
     while ((c = getchar()) != EOF)  {
-        ++nc;
-        if (c == '\n')
-            ++nl;
-        if (c == ' ' || c == '\n' || c == '\t')
-            state = OUT;
-        else if (state == OUT)  {
-            state = IN;
-            ++nw;
-        }
+        if (c == ' ' || c == '\t')      /* If the character is a space or tab, output a newline */
+            putchar('\n');
+        else
+            putchar(c);                 /* Otherwise, just print the character */
     }
-    printf("%d %d %d\n", nl, nw, nc);
 }

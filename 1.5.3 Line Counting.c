795408842d7b6faab1b1a180b8111fac3d0793ab
Peter Pyne <pyne.peter@gmail.com>
2017-08-26 20:04:38 -0400

    Modified for exercise 1-9. Which was hard.

--- 1.5.3 Line Counting.c
+++ 1.5.3 Line Counting.c
@@ -1,26 +1,29 @@
 //
 // Created by Peter Pyne on 8/26/2017.
 //
-// ***Warning. If you're developing in a Windows environment, this task WILL give you problems for two reasons
+// ***Warning. If you're developing in a Windows environment, this task WILL give you problems.
 // 1 - Nowhere in the book does it tell you to press CTRL+Z to input an EOF. Do that to break out of the while loop.
 // It ONLY works in CMD, not in any IDE's like Clion, so you actually have to compile it first.
-// 2 - CMD will not accept a 'newline' character. I've tried. Just substitute it for tab ('\t') to get the gist
-// of the script, as I have done here.
-// Further reading: https://stackoverflow.com/questions/3646255/problem-with-example-1-5-2-in-kr-book-on-c
 
 #include <stdio.h>
 
-/* count tabs and spaces in input as per Exercise 1-8 */
+/* Copy input to output, replace each string of one or more blanks with a single blank as per Ex 1-9*/
+/* I had to Google for assistance with this one. Managed to get the right code, but needed the hint */
+/* to call getchar() a second time to ignore consecutive spaces. Makes sense to me now. */
+
 main()
 {
-    int c, tab, space;
-
+    int space, c;
     space = 0;
-    tab = 0;
+
     while ((c = getchar()) != EOF)
-        if (c == '\t')
-            ++tab;
-        else if (c == ' ')
-            ++space;
-    printf("%d tabs, %d spaces", tab, space);
+        if (c == ' ')
+            {
+            while ((c = getchar()) == ' ')  /* Testing if the first character after the 1st space is another space */
+                ;                           /* If it is, we do nothing (ignore the space and ask again) */
+            printf(" ");                    /* If it isn't, we print the initial space */
+            putchar(c);                     /* Followed by the input that broke us out of the second while loop */
+            }
+        else
+            putchar(c);                     /* We print non spaces normally. */
 }

61bb4e5625a4dd2c6922f31155771ade538957ff
Peter Pyne <pyne.peter@gmail.com>
2017-08-28 12:25:26 -0400

    Initial commit

---
+++ 1.7 Functions.c
@@ -0,0 +1,29 @@
+//
+// Created by Peter Pyne on 8/28/2017.
+//
+
+#include <stdio.h>
+
+int power(int m, int n);
+
+/* test power function */
+
+main()
+{
+    int i;
+
+    for (i = 0; i < 10; ++i)
+        printf("%d %d %d\n", i, power(2,i), power(-3,i));
+    return 0;
+}
+
+/* power: raise base to n=th power; n >= 0 */
+int power(int base, int n)
+{
+    int i, p;
+
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+    return p;
+}

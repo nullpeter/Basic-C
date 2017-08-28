4cfb8ccadfe5ef3fd0ed4c16b619753b2510bbca
Peter Pyne <pyne.peter@gmail.com>
2017-08-28 11:19:16 -0400

    Modified for exercise 1-14.

--- 1.6 Arrays.c
+++ 1.6 Arrays.c
@@ -4,14 +4,14 @@
 
 #include <stdio.h>
 
-/* Exercise 1-13: Write a program to print a histogram of the lengths of words in its input. */
+/* Exercise 1-14: Write a program to print a histogram of the frequencies of different characters in its input. */
 
 main()
 {
-    int c, i, charcount;
+    int c, i, charcount, rcount;
     int wlenth[20];                     /* Create an array of common word lengths (1 - 20) */
 
-    c = i = charcount = 0;              /* Initialize the values */
+    i = c = charcount = rcount = 0;              /* Initialize the values */
 
     for (i = 0; i < 20; ++i)            /* Prefills with 0's */
         wlenth[i] = 0;
@@ -19,18 +19,28 @@ main()
 /* Count the length of a word */
 
     while ((c = getchar()) != EOF)
-        if (c != ' ' && c != '\n' && c != '\t') /* Count each letter unless the letter is whitespace / tab / newline */
+        if (c == 'r') {
+            rcount++;
+        }
+        else if (c != ' ' && c != '\n' && c != '\t') /* Count each letter unless the letter is whitespace / tab / newline */
             charcount++;
         else {
             wlenth[charcount]++;        /* Store the result in the array */
             charcount = 0;              /* Reset the counter */
         }
+    if (rcount == 1)
+        printf("There is 1 r in your input.");
+    else if (rcount >= 1)
+        printf("There are %d r's in your input.", rcount);
+    else
+        printf("\nThere are no r's in your input.");
+    printf("\n\nH i s t o g r a m\n\n");
+
     for (i = 1; i < 20; ++i) {
         printf("%2d: ", i);
         while (wlenth[i]) {
             printf("|");
             wlenth[i]--;
-
         }
     printf("\n");
     }
